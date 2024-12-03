cd6c66bb2f7f90415ae5e7e5675ee3ad70611d4e
Manyyy20 <manuel.ruiz@cetys.edu.mx>
2024-12-02 21:23:19 -0800

    Done Last Step Add Security SQL Server IP

--- deploy_script.sh
+++ deploy_script.sh
@@ -9,8 +9,7 @@ SQL_SERVER_NAME="snakegamesqlserver"
 SQL_DATABASE_NAME="snakeGameDatabase"
 SQL_ADMIN_USERNAME="sqladmin"
 SQL_ADMIN_PASSWORD="Password22"
-CURRENT_IP="4.152.25.8"
-REAL_IP="77.247.126.140"
+
 # Desplegar recursos con ARM Template
 echo "Desplegando recursos con ARM Template..."
 az deployment group create --resource-group $RESOURCE_GROUP --template-file $ARM_TEMPLATE
@@ -21,6 +20,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Obtener la IP pública actual correcta
+CURRENT_IP=$(curl -s https://api.ipify.org)
 
 # Configurar reglas de firewall en SQL Server
 echo "Configurando reglas de firewall en SQL Server..."
@@ -31,13 +31,6 @@ az sql server firewall-rule create \
     --name AllowCurrentIP \
     --start-ip-address $CURRENT_IP \
     --end-ip-address $CURRENT_IP
-
-az sql server firewall-rule create \
-    --resource-group $RESOURCE_GROUP \
-    --server $SQL_SERVER_NAME \
-    --name AllowCurrentIP \
-    --start-ip-address $REAL_IP \
-    --end-ip-address $REAL_IP
 
 if [ $? -ne 0 ]; then
     echo "Error: No se pudieron configurar las reglas de firewall."
@@ -62,7 +55,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 cd ..
-#test
+
 # Configurar variables de entorno adicionales para la Function App
 echo "Configurando variables de entorno adicionales para Function App..."
 az functionapp config appsettings set --name $FUNCTION_APP_NAME --resource-group $RESOURCE_GROUP --settings \
